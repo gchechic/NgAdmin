428932515b6fc655eceb9b2434c9d8a517dd0abc
gchechic <gchechic@yahoo.com>
2018-05-23 09:12:52 -0300

    clean error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  AfterViewChecked
+} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { LoginService } from './login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'adm-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy, AfterViewChecked {
   showSpinner = false;
   username: string;
   password: string;
   errorMsg: string;
 
+  private loginForm: NgForm;
+  private subscrs: Subscription;
+  @ViewChild('loginForm') currentForm: NgForm;
+
   constructor(private loginService: LoginService) {}
 
   ngOnInit() {
     this.username = null;
-    this.errorMsg = 'Error!';
+    this.errorMsg = null;
   }
+  ngOnDestroy() {
+    if (this.subscrs) {
+      this.subscrs.unsubscribe();
+    }
+  }
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    if (this.currentForm === this.loginForm) {
+      return;
+    }
+    this.loginForm = this.currentForm;
+    if (this.currentForm) {
+      this.subscrs = this.currentForm.valueChanges.subscribe(data => {
+        this.errorMsg = null;
+      });
+    }
+  }
+
   login() {
     this.showSpinner = true;
     this.loginService.login(this.username, this.password).subscribe(
