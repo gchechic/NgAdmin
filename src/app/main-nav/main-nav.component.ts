dcea3d526f7bfcde027116244db20e27c26eab5a
gchechic <gchechic@yahoo.com>
2018-05-30 10:56:02 -0300

    modelo 1 sidenav

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -5,15 +5,21 @@ import {
   BreakpointState
 } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'adm-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(
-    Breakpoints.Handset
-  );
+  isHandset: boolean;
+  isDarkTheme = false;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      tap(result => (this.isHandset = result.matches)),
+      map(result => result.matches)
+    );
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
--- src/app/procesos/procesos-routing.module.ts
+++ src/app/procesos/procesos-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProcesosListComponent } from './procesos-list/procesos-list.component';
+import { ProcesosComponent } from './procesos.component';
 const routes: Routes = [
   {
     path: '',
-    component: ProcesosListComponent
+    children: [
+      {
+        path: '',
+        component: ProcesosComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        component: ProcesosListComponent
+      }
+    ]
   }
 ];
 
